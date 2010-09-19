8a9434ae83bb672598c54eaed57b407d4fce4541
christoph heindl <christoph.heindl@gmail.com>
2010-09-18 22:49:02 -0700

    added random test

--- test/unit/test_similarity.rb
+++ test/unit/test_similarity.rb
@@ -35,5 +35,29 @@ class TestSimilarity < Test::Unit::TestCase
     assert_equal(1.0, PMK.similarity_normed(levels_root, levels_root, :nlevels => 1))
   end
   
+  def test_normed_similarity_random
+    n = 100
+    d = 10
+    levs = 10
+    
+    #box = PMK::Box.new([-0.5]*d, [1.5]*d)
+    box = PMK::Box.new([-0.5]*d, [1.5]*d)
+    box.lower.map! {|e| e - 0.5 + rand()*1.0}
+    box.upper.map! {|e| e - 0.5 + rand()*1.0}
+    
+    subdiv = PMK::Subdivision.new(PMK::SubdivisionController.new(:nlevels => levs))
+    pyr = PMK::PyramideView.new(:nlevels => levs, :ndims => d)
+    
+    srand(10)
+    f_a = Array.new(n) {Array.new(d){rand()}}   
+    f_b = f_a.map {|f| f.map {|e| e + rand()*0.001} }
+    puts ""
+    
+    pv_a = pyr.generate(subdiv.generate(f_a, box))
+    pv_b = pyr.generate(subdiv.generate(f_b, box))
+    
+    puts PMK.similarity_normed(pv_a, pv_b)
+  end
+  
   
 end
